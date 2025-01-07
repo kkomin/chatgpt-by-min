cde1db7efd331be40b1d7aa4d2149c9eac450663
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-07 21:50:35 +0900

    ⚙️로그인 필드 생성성

--- types/form.ts
+++ types/form.ts
@@ -2,4 +2,9 @@ export type TSignUpFormError = {
     name?: string[];
     email?: string[];
     password?: string[];
+};
+
+export type TLoginFormError = {
+    email?: string[];
+    password?: string[];
 }
