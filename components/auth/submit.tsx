628c2a3cb3f067724522feb267a3edff381a4efb
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-07 22:32:52 +0900

    🔧 버튼에 disable 추가

--- components/auth/submit.tsx
+++ components/auth/submit.tsx
@@ -1,8 +1,10 @@
-import { Children } from "react";
+import { useFormStatus } from "react-dom";
 import { Button, ButtonProps } from "../ui/button";
 
 export function Submit({ children, ...others}: ButtonProps){
+    const {pending} = useFormStatus();
+
     return (
-        <Button {...others} type="submit">{children}</Button>
+        <Button {...others} type="submit" disabled={pending}>{children}</Button>
     );
 }
