abdfd029f467e5ce701b98a0816b4ddb0a33cfef
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-07 12:31:50 +0900

    ⚙️회원가입 form 추가가

--- components/auth/SignUpForm.tsx
+++ components/auth/SignUpForm.tsx
@@ -6,7 +6,15 @@ export default function SignUpForm() {
         title="회원가입" 
         footer={{ label: "이미 계정이 있으신가요?", href: "/login" }}>
             <form>
-                
+                {/* 이름 */}
+                <div>
+                </div>
+                {/* 이메일 */}
+                <div>
+                </div>
+                {/* 비밀번호 */}
+                <div>
+                </div>
             </form>
     </FormCard>
     );
